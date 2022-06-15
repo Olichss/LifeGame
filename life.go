d5d50f2e1caa1818fcafcc25c8a7147599ac0bde
Olich <sergienkoolich@yandex.ru>
2022-06-15 14:40:38 +0300

    Добавлена остановка игры, логика

--- life.go
+++ life.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
 
 const (
 	width  = 80
@@ -10,22 +20,96 @@ const (
 type Universe [][]bool
 
 func NewUniverse() Universe {
-	a := make([][]bool, height)
-	for i := range a {
-		a[i] = make([]bool, width)
+	u := make(Universe, height)
+	for i := range u {
+		u[i] = make([]bool, width)
 	}
-	return Universe(a)
+	return u
+}
+
+// Seed заполняет вселенную случайными живыми клетками.
+func (u Universe) Seed() {
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < (width * height / 4); i++ {
+		u[rand.Intn(height)][rand.Intn(width)] = true
+	}
+}
+
+func normilizeCoord(x, size int) int {
+	if x < 0 {
+		return normilizeCoord(x+size, size)
+	}
+	return x % size
+}
+
+func (u Universe) Alive(x, y int) bool {
+	// x = (x + width) % width
+	// y = (y + height) % height
+
+	x = normilizeCoord(x, width)
+	y = normilizeCoord(y, height)
+	return u[y][x]
+}
+
+// Neighbors подсчитывает прилегающие живые клетки.
+func (u Universe) Neighbors(x, y int) int {
+	n := 0
+	for v := -1; v <= 1; v++ {
+		for h := -1; h <= 1; h++ {
+			if !(v == 0 && h == 0) && u.Alive(x+h, y+v) {
+				n++
+			}
+		}
+	}
+	return n
+}
+
+func (u Universe) Next(x, y int) bool {
+	n := u.Neighbors(x, y)
+	return n == 3 || n == 2 && u.Alive(x, y)
+}
+
+func (u Universe) String() string {
+	var b byte
+	buf := make([]byte, 0, (width+1)*height)
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			b = ' '
+			if u[y][x] {
+				b = '*'
+			}
+			buf = append(buf, b)
+		}
+		buf = append(buf, '\n')
+	}
+
+	return string(buf)
 }
 
 func (u Universe) Show() {
-	for _, rows := range u {
-		for _, cell := range rows {
-			if cell {
-				fmt.Print("+")
-			} else {
-				fmt.Print("-")
+	fmt.Print("\x0c", u.String())
+	fmt.Println("---------------------------------------------------------------------------")
+}
+
+func (u Universe) isAlive() bool {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if u[y][x] {
+				return true
 			}
 		}
-		fmt.Println()
 	}
+
+	return false
+}
+
+func Step(a, b Universe) bool {
+	for y := 0; y < width; y++ {
+		for x := 0; x < height; x++ {
+			b[x][y] = a.Next(x, y)
+		}
+	}
+
+	return b.isAlive()
 }
--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
-	u := NewUniverse()
-	u.Show()
+	rand.Seed(time.Now().UnixNano())
+	a, b := NewUniverse(), NewUniverse()
+	a.Seed()
+
+	for {
+		if !Step(a, b) {
+			fmt.Println("Жизнь умерла")
+		}
+		a.Show()
+		time.Sleep(time.Second / 30)
+		a, b = b, a
+	}
+
 }
